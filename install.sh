d35bba70b3bb9c68a49d568dea7829df2f1f2240
Jesus Garcia <jeg114@ic.ac.uk>
2017-11-05 17:04:06 +0000

    pv package

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@ SETUP_DIR=`echo $0 | sed -r "s/(.*)install.sh.*/\1/"`
 
 echo "Setting up bashrc"
 cat ${SETUP_DIR}/append.bashrc >> ~/.bashrc
+source ~/.bashc
 
 echo "Setting up vimrc"
 cat ${SETUP_DIR}/append.vimrc >> ~/.vimrc
 
 echo "Setting up packages"
 sudo apt-get update
-sudo apt-get install --yes --force-yes g++ make libtbb-dev bc
+sudo apt-get install --yes --force-yes g++ make libtbb-dev bc pv
 
