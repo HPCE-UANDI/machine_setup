45a8089c96ddbffabff51b7346e98d34f67d021a
Ubuntu <ubuntu@ip-172-31-36-67.ec2.internal>
2017-12-28 14:07:27 +0000

    [deetz] made install

---
+++ install_deetz.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+SETUP_DIR=`echo $0 | sed -r "s/(.*)install_deetz.sh.*/\1/"`
+DIR=$(pwd)/${SETUP_DIR}
+echo $DIR
+
+echo "Setting up packages"
+sudo apt-get update
+sudo apt-get install --yes --force-yes g++ make libtbb-dev bc pv
+# sudo apt-get install zsh 
+# sudo (chsh -s $(which-zsh)
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+mv vim-colors-solarized ~/.vim/bundle/
+
+echo "Setting up bashrc"
+cat ${DIR}/append.bashrc > ~/.bashrc
+source ~/.bashrc
+
+echo "Setting up vimrc"
+cat ${DIR}/deetz/append.vimrc > ~/.vimrc
