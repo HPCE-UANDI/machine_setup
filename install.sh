205eb5306925d6cd5a836e80ec910de6b584f5ea
Ubuntu <ubuntu@ip-172-31-41-246.ec2.internal>
2017-12-28 00:02:58 +0000

    [deetz] updated

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 SETUP_DIR=`echo $0 | sed -r "s/(.*)install.sh.*/\1/"`
 
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+mv vim-colors-solarized ~/.vim/bundle/
+
 echo "Setting up bashrc"
 cat ${SETUP_DIR}/append.bashrc >> ~/.bashrc
 source ~/.bashc
@@ -10,4 +17,3 @@ cat ${SETUP_DIR}/append.vimrc >> ~/.vimrc
 echo "Setting up packages"
 sudo apt-get update
 sudo apt-get install --yes --force-yes g++ make libtbb-dev bc pv
-
